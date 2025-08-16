b1386c56341b1f49e1311c4ac9da8dd1fadc3fff
edwardtsatsu <edwardakorlie73@gmail.com>
2025-08-16 03:02:21 +0000

    fixed issueson the update

--- apps/books/src/books/books.controller.ts
+++ apps/books/src/books/books.controller.ts
@@ -3,33 +3,34 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { BooksService } from './books.service';
 import { CreateBookDto } from '@app/contracts/books/create-book.dto';
 import { UpdateBookDto } from '@app/contracts/books/update-book.dto';
+import { BOOKS_PATTERNS } from '@app/contracts/books/books.patterns';
 
 
 @Controller()
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
-  @MessagePattern('books.create')
+  @MessagePattern(BOOKS_PATTERNS.CREATE)
   create(@Payload() createBookDto: CreateBookDto) {
     return this.booksService.create(createBookDto);
   }
 
-  @MessagePattern('books.findAll')
+  @MessagePattern(BOOKS_PATTERNS.FIND_ALL)
   findAll() {
     return this.booksService.findAll();
   }
 
-  @MessagePattern('books.findOneBook')
+  @MessagePattern(BOOKS_PATTERNS.FIND_ONE)
   findOne(@Payload() id: number) {
     return this.booksService.findOne(id);
   }
 
-  @MessagePattern('books.update')
+  @MessagePattern(BOOKS_PATTERNS.UPDATE)
   update(@Payload() updateBookDto: UpdateBookDto) {
     return this.booksService.update(updateBookDto.id, updateBookDto);
   }
 
-  @MessagePattern('books.remove')
+  @MessagePattern(BOOKS_PATTERNS.REMOVE)
   remove(@Payload() id: number) {
     return this.booksService.remove(id);
   }
--- apps/bookstore-api-gateway/src/books/books.service.ts
+++ apps/bookstore-api-gateway/src/books/books.service.ts
@@ -1,8 +1,13 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+import { BOOKS_PATTERNS } from '@app/contracts/books/books.patterns';
+import { BookDto as ClientBookDto } from '@app/contracts/books/book.dto';
+import { CreateBookDto as ClientCreateBookDto } from '@app/contracts/books/create-book.dto';
+import { UpdateBookDto as ClientUpdateBookDto } from '@app/contracts/books/update-book.dto';
 import { CreateBookDto } from '@app/contracts/books/create-book.dto';
 import { UpdateBookDto } from '@app/contracts/books/update-book.dto';
-import { Inject, Injectable } from '@nestjs/common';
 
-import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class BooksService {
@@ -10,23 +15,34 @@ export class BooksService {
 
   create(createBookDto: CreateBookDto) {
     // return 'This action adds a new book';
-    return this.booksClient.send('books.create', createBookDto);
+    return this.booksClient.send<ClientBookDto, ClientCreateBookDto>(
+      BOOKS_PATTERNS.CREATE, 
+      createBookDto
+    );
   }
 
   findAll() {
-    // return `This action returns all books`;
-    return this.booksClient.send('books.findAll', {})
+    return this.booksClient.send<ClientBookDto>(
+      BOOKS_PATTERNS.FIND_ALL, {})
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} book`;
+    return this.booksClient.send<ClientBookDto>(
+      BOOKS_PATTERNS.FIND_ONE, { id }
+    );
   }
 
   update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
+    return this.booksClient.send<ClientBookDto, { id: number; data: ClientUpdateBookDto }>(
+      BOOKS_PATTERNS.UPDATE,
+      { id, data: updateBookDto },
+    );
   }
+  
 
   remove(id: number) {
-    return `This action removes a #${id} book`;
+    return this.booksClient.send<ClientBookDto>(
+      BOOKS_PATTERNS.REMOVE, { id }
+    );
   }
 }
