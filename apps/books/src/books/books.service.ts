5d0b0ce24ea9e84e0c087b6fc5fc42d825404972
edwardtsatsu <edwardakorlie73@gmail.com>
2025-08-16 02:00:58 +0000

    added the bookimg service for this

--- apps/books/src/books/books.service.ts
+++ apps/books/src/books/books.service.ts
@@ -1,16 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { BookDto } from './dto/book.dto';
 
 @Injectable()
 export class BooksService {
+  private books: BookDto[] = [
+    {
+      id: 1,
+      title: 'Book 1',
+      author: 'Author 1',
+      price: 22,
+      publicationDate: new Date(),
+      publisher: 'Publisher 1',
+      rating: 'Rating 1',
+      description: 'Description 1',
+    },
+    {
+      id: 2,
+      title: 'Book 2',
+      author: 'Author 2',
+      price: 33,
+      publicationDate: new Date(),
+      publisher: 'Publisher 2',
+      rating: 'Rating 2',
+      description: 'Description 2'
+    },
+  ];
+
   create(createBookDto: CreateBookDto) {
     console.log('This action adds a new book');
-    
+
+    const newBook: BookDto = {
+      id: this.books.length + 1,
+      ...createBookDto,
+    }
+
+    this.books.push
+    return newBook;
   }
 
   findAll() {
-    return `This action returns all books`;
+    return this.books;
   }
 
   findOne(id: number) {
